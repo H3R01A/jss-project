8e9e82b1468265d769eb38e6e65a4884853808a6
H3R01A <aallen0001@gmail.com>
2025-04-11 10:49:08 -0700

    update main page with links to navigate to different features

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,12 @@
-/*
-
-Recreate ChatGPT search
-Next.js with TypeScript and JigsawStack
-
-- scrapes/searches internet for subject matter
-- provide response on what you searched for while also citing where it retreived info
-
-*/
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-    <h1 className="text-lg">Hello World</h1>
+    <div className="flex flex-col gap-4 items-center justify-items-center min-h-screen sm:p-10 font-[family-name:var(--font-geist-sans)]">
+      <h1 className="text-lg">Welcome to the JSS Super App</h1>
+      <Link href="/search">JSS Search</Link>
+      <Link href="/prompt-game">JSS Adventure Game</Link>
+      <Link href="/aspect-ratio">JSS Aspect Ratio Calculator</Link>
     </div>
   );
 }
