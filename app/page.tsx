200f8f7832bc325cc30564d3e2b1f7b9a928da26
H3R01A <aallen0001@gmail.com>
2025-04-11 11:56:02 -0700

    update main page for styling

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div className="flex flex-col gap-4 items-center justify-items-center min-h-screen sm:p-10 font-[family-name:var(--font-geist-sans)]">
-      <h1 className="text-lg">Welcome to the JSS Super App</h1>
+      <h1 className="text-3xl">Welcome to the JSS Super App</h1>
       <h2 className="text-lg">Click a feature below to get started</h2>
       <Link href="/search">JSS Search</Link>
       <Link href="/prompt-game">JSS Adventure Game</Link>
