de7955b11d625ccad696a67c68b6dd3712dadfdd
H3R01A <aallen0001@gmail.com>
2025-04-11 10:50:28 -0700

    update main page subtitle to reflect new features

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col gap-4 items-center justify-items-center min-h-screen sm:p-10 font-[family-name:var(--font-geist-sans)]">
       <h1 className="text-lg">Welcome to the JSS Super App</h1>
+      <h2 className="text-lg">Click a feature below to get started</h2>
       <Link href="/search">JSS Search</Link>
       <Link href="/prompt-game">JSS Adventure Game</Link>
       <Link href="/aspect-ratio">JSS Aspect Ratio Calculator</Link>
