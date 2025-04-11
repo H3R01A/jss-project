7ddb44ff22ec13dcc62e48823ca6b50b07242a74
H3R01A <aallen0001@gmail.com>
2025-04-11 11:55:44 -0700

    add back prompt engine creation for testing

--- app/api/prompt/route.ts
+++ app/api/prompt/route.ts
@@ -9,34 +9,32 @@ export async function POST(req: Request) {
 
   try {
 
-    // const params = {
-    //   prompt:
-    //     "You are a level 50 Minotaur with an axe. Given this player's level:{level} and weapon:{weapon}, determine if they are able to beat you and win the game",
-    //   inputs: [
-    //     {
-    //       key: 'level',
-    //       optional: false,
-    //     },
-    //     {
-    //       key: 'weapon',
-    //       optional: false,
-    //     },
-    //   ],
-    //   return_prompt: {
-    //     canBeatYouWithLevel:
-    //       'Return true if the level is higher than your level. Else return false',
-    //     canBeatYouWithSacredWeapon:
-    //       'Return true if the weapon is a sacred weapon. Else return false',
-    //   },
-    // };
+    const params = {
+      prompt:
+        "You are a level 50 Minotaur with an axe. Given this player's level:{level} and weapon:{weapon}, determine if they are able to beat you and win the game",
+      inputs: [
+        {
+          key: 'level',
+          optional: false,
+        },
+        {
+          key: 'weapon',
+          optional: false,
+        },
+      ],
+      return_prompt: {
+        canBeatYouWithLevel:
+          'Return true if the level is higher than your level. Else return false',
+        canBeatYouWithSacredWeapon:
+          'Return true if the weapon is a sacred weapon. Else return false',
+      },
+    };
 
-    // const result = await jigsawstack.prompt_engine.create(params);
-    // const prompt_id = result.prompt_engine_id;
-
-    // console.log(prompt_id);
+    const result = await jigsawstack.prompt_engine.create(params);
+    const prompt_id = result.prompt_engine_id;
 
     const finalResult = await jigsawstack.prompt_engine.run({
-      id: `6d0c1ae0-68fe-48ce-a165-4576480d3753`,
+      id: prompt_id,
       input_values: {
         level: playerLevel,
         weapon: playerWeapon,
